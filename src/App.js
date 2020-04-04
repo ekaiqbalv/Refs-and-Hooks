6d2a66d3a7574611d07d145c7bc0c6c6416ba6a5
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2020-04-04 17:40:55 +0700

    Using react refs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.textInput1 = React.createRef();
+    this.textInput2 = React.createRef();
+
+    this.state = { value: "" };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ value: this.textInput1.current.value });
+    console.log(this.textInput2.current.value);
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>React Ref - createRef</h2>
+        <h3>Value: {this.state.value}</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" ref={this.textInput1} />
+          <input type="text" ref={this.textInput2} />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
